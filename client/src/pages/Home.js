5cffa861fe3152484a1dfdc36af53fdcffc276a0
sk7001 <sk7001@srmist.edu.in>
2024-09-10 14:33:59 +0530

    loginlink

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <main>
-      
+      <Link to={'/login'} style={{color:'white'}}>Login</Link>
     </main>
   )
 }
