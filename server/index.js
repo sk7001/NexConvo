d78b2e0a55a728962edc863a810ba5f3461cf8d1
sk7001 <sk7001@srmist.edu.in>
2024-09-22 04:59:20 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const http = require("http");
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: [process.env.FRONTEND_URL,'https://nexconvo.netlify.app'],
+        origin: [process.env.FRONTEND_URL],
         methods: ["GET", "POST"],
         credentials: true
     }
